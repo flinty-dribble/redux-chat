d3ee6a0fd987c6d8089d826069eb2ec45ee6ca0e
Mirab <you@example.com>
2022-09-14 21:43:26 +0300

    tests

--- src/reduxChat.ts
+++ src/reduxChat.ts
@@ -1,5 +1,38 @@
 import { createStore } from "redux";
 
+const config = {
+  firebaseBaseUrl: "https://otus-js-chat-4ed79-default-rtdb.firebaseio.com",
+  firebaseCollection: "messages.json",
+};
+
+export async function getMessagesList() {
+  try {
+    return fetch(`${config.firebaseBaseUrl}/${config.firebaseCollection}`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then(
+        (data: {
+          el: {
+            date: string;
+            message: string;
+            name: string;
+            nickname?: string;
+          };
+        }) =>
+          Object.values(data).map((el) => ({
+            ...el,
+            date: new Date(el.date),
+          }))
+      );
+  } catch (err: any) {
+    return err;
+  }
+}
+
 type Reducer<State, Action> = (state: State | any, action: Action) => State;
 
 const initialState = {
@@ -30,59 +63,40 @@ const chatApp: Reducer<
       return state;
   }
 };
-const store = createStore(chatApp);
 
-const config = {
-  firebaseBaseUrl: "https://otus-js-chat-4ed79-default-rtdb.firebaseio.com",
-  firebaseCollection: "messages.json",
-};
+export const store = createStore(chatApp);
 
-async function getMessagesList() {
-  return fetch(`${config.firebaseBaseUrl}/${config.firebaseCollection}`, {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then(
-      (data: {
-        el: { date: string; message: string; name: string; nickname?: string };
-      }) =>
-        Object.values(data).map((el) => ({
-          ...el,
-          date: new Date(el.date),
-        }))
-    );
-}
+export async function sendMessage(data: { name: string; message: string }) {
+  try {
+    const actionAdd = {
+      type: "ADD_MESSAGE",
+      name: data.name,
+      message: data.message,
+    };
 
-async function sendMessage(data: { name: string; message: string }) {
-  const actionAdd = {
-    type: "ADD_MESSAGE",
-    name: data.name,
-    message: data.message,
-  };
+    store.dispatch(actionAdd);
 
-  store.dispatch(actionAdd);
+    const actionHide = {
+      type: "HIDE_MESSAGE",
+      index: store.getState().messages.length - 6,
+    };
 
-  const actionHide = {
-    type: "HIDE_MESSAGE",
-    index: store.getState().messages.length - 6,
-  };
+    store.dispatch(actionHide);
 
-  store.dispatch(actionHide);
-
-  return fetch(`${config.firebaseBaseUrl}/${config.firebaseCollection}`, {
-    method: "POST",
-    body: JSON.stringify({
-      ...data,
-      date: new Date(),
-    }),
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  }).then((response) => response.json());
+    return fetch(`${config.firebaseBaseUrl}/${config.firebaseCollection}`, {
+      method: "POST",
+      body: JSON.stringify({
+        ...data,
+        date: new Date(),
+      }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then((response) => response.json());
+  } catch (err) {
+    return err;
+  }
 }
 
 const body = document.querySelector("body");
@@ -119,43 +133,58 @@ body?.append(button);
 
 document.querySelectorAll("p")[0].innerHTML = "nickname: ";
 document.querySelectorAll("p")[1].innerHTML = "message: ";
+
 button.innerHTML = "send";
-
 function addMessage() {
-  for (let y = 0; y < 5; y += 1) {
-    const action = {
-      type: "ADD_MESSAGE",
-      name: document.querySelectorAll(".name")[y].innerHTML,
-      message: document.querySelectorAll(".text")[y].innerHTML,
-    };
+  try {
+    for (let y = 0; y < 5; y += 1) {
+      const action = {
+        type: "ADD_MESSAGE",
+        name: document.querySelectorAll(".name")[y].innerHTML,
+        message: document.querySelectorAll(".text")[y].innerHTML,
+      };
 
-    store.dispatch(action);
+      store.dispatch(action);
+    }
+  } catch (err) {
+    return err;
   }
-  console.log(store.getState());
 }
 
-getMessagesList().then((res) => {
-  //console.log(res)
-  for (let i = res.length - 5; i <= res.length - 1; i += 1) {
-    const messageWrapper = document.createElement("div");
-    const nickname = document.createElement("p");
-    const message = document.createElement("p");
+export async function firstFiveMessages() {
+  await getMessagesList().then((res) => {
+    try {
+      for (let i = res.length - 5; i <= res.length - 1; i += 1) {
+        const messageWrapper = document.createElement("div");
+        const nickname = document.createElement("p");
+        const message = document.createElement("p");
 
-    messageWrapper.classList.add("message");
-    nickname.classList.add("name");
-    message.classList.add("text");
+        messageWrapper.classList.add("message");
+        nickname.classList.add("name");
+        message.classList.add("text");
 
-    body?.append(messageWrapper);
-    messageWrapper?.append(nickname);
-    messageWrapper?.append(message);
+        body?.append(messageWrapper);
+        messageWrapper?.append(nickname);
+        messageWrapper?.append(message);
 
-    nickname.innerHTML = `nickname: ${res[i].name}`;
-    message.innerHTML = `message: ${res[i].message}`;
-  }
-  addMessage();
-});
+        if (res[i].nickname === undefined) {
+          nickname.innerHTML = res[i].name;
+        } else {
+          nickname.innerHTML = res[i].nickname;
+        }
+        message.innerHTML = res[i].message;
+      }
 
-button?.addEventListener("click", () => {
+      addMessage();
+    } catch (err) {
+      return err;
+    }
+  });
+}
+
+firstFiveMessages();
+
+export async function buttonFunc() {
   sendMessage({
     name: `${inputNickname.value}`,
     message: `${inputMessage.value}`,
@@ -163,26 +192,7 @@ button?.addEventListener("click", () => {
 
   document.querySelectorAll(".message").forEach((e) => e.remove());
 
-  getMessagesList().then((res) => {
-    for (let i = res.length - 5; i <= res.length - 1; i += 1) {
-      const messageWrapper = document.createElement("div");
-      const nickName = document.createElement("p");
-      const message = document.createElement("p");
+  await firstFiveMessages();
+}
 
-      messageWrapper.classList.add("message");
-
-      body?.append(messageWrapper);
-      messageWrapper?.append(nickName);
-      messageWrapper?.append(message);
-
-      if (res[i].nickname === undefined) {
-        nickName.innerHTML = `nickname: ${res[i].name}`;
-      } else {
-        nickName.innerHTML = `nickname: ${res[i].nickname}`;
-      }
-
-      message.innerHTML = `message: ${res[i].message}`;
-    }
-    console.log(store.getState());
-  });
-});
+button?.addEventListener("click", buttonFunc);
